b5039dce8d62eeda1277fac7d9cbf052740b14ce
Red Horn <redhorn@Reds-MacBook-Pro.local>
2018-07-11 16:48:10 -0700

    Start fresh

--- app/controllers/hello_world_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class ValidatorController < ApplicationController
-  # layout "hello_world"
-
-  def index
-
-  end
-end
--- app/controllers/validator_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ValidatorController < ApplicationController
-  def index
-    @hello_world_props = { name: "Stranger" }
-  end
-end
